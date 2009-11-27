f535cf8e689d4f2ac84f4ef0ed51f4dfdd7d21f2
cofitzpa <cofitzpa@4525493e-7705-40b1-a816-d608a930855b>
2009-11-27 13:00:21 +0000

    modified config file for swave

--- scripts/runsWave.sh
+++ scripts/runsWave.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-export ROOTSYS=/Disk/lochnagar0/general/root/5.24.00_slc5_gcc34_bin
+export ROOTSYS=/Disk/lochnagar0/general/root/5.25.04_gcc41/
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ROOTSYS}/lib
 export PYTHONPATH=${ROOTSYS}/lib:${PYTHONPATH}
 export PATH=${PATH}:${ROOTSYS}/bin
