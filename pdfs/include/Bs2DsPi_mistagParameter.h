9650f9a7eea84b0ea5883769398362d3cf0bb204
clarke <clarke@4525493e-7705-40b1-a816-d608a930855b>
2009-12-16 12:38:08 +0000

    DsPi PDF form PELC. With resolution

---
+++ pdfs/include/Bs2DsPi_mistagParameter.h
@@ -0,0 +1,88 @@
+/** @class Bs2DsPi_mistagParameter Bs2DsPi_mistagParameter.h
+ *
+ *  RapidFit PDF for Bs2DsPi
+ *
+ *  Modified by Pete to make mistag a fit parameter and add time resolution
+ *
+ *  @author Gemma Fardell
+ */
+
+#ifndef Bs2DsPi_mistagParameter_H
+#define Bs2DsPi_mistagParameter_H
+
+#include "BasePDF.h"
+#include "RooComplex.h"
+#include "RooMath.h"
+
+class Bs2DsPi_mistagParameter : public BasePDF
+{
+	public:
+		Bs2DsPi_mistagParameter();
+		~Bs2DsPi_mistagParameter();
+
+		//Calculate the PDF value
+		virtual double Evaluate(DataPoint*);
+		virtual bool SetPhysicsParameters(ParameterSet*);
+		virtual vector<string> GetDoNotIntegrateList();
+
+	protected:
+		//Calculate the PDF normalisation
+		virtual double Normalisation(DataPoint*, PhaseSpaceBoundary*);
+
+	private:
+		void MakePrototypes();
+
+		// These contain the strings that correspond
+		// to the physics parameter names that will be
+		// used in the minimiser.
+		string gammaName;		// gamma
+		string deltaGammaName;	// delta gamma
+		string deltaMName;		// delta mass
+    	string mistagName;		// B mistag
+	    string timeresName;		// B mistag
+
+		// These contain the strings that correspond
+		// to the observable names that are used in the
+		// PDF. 
+		string timeName;		// proper time
+		string tagName;		// B tag
+	   
+		// Physics parameters
+	    double gamma, deltaGamma, deltaM, mistag, timeRes ;
+	
+	    // Observables
+		double time ;
+		int tag;
+
+	    // Limits
+		double tlow, thigh ;
+	
+		void getPhysicsParameters(  );
+	    void getObservables( DataPoint* );
+		
+		void getTimeDependentFuncsInt(double&, double&, double&, PhaseSpaceBoundary*);
+	
+		// Widths
+		double gamma_l() const ;
+		double gamma_h() const ;
+		double gambar() const ;
+	
+		// Time primitives
+		double expL() const ;
+		double expH() const ;
+		double expCos() const ;
+		double expLint() const ;
+		double expHint() const ;
+		double expCosInt() const ;
+		
+		// Functions to help convolve a single gaussian into time primitives
+		// DIDNT APPEAR TO BE USED RooComplex evalCerf( double, double, double ) const ;
+		RooComplex evalCerfApprox( double, double, double ) const ;
+		double evalCerfRe( double, double, double ) const ;
+		double evalCerfIm( double, double, double ) const ;
+	
+	
+
+};
+
+#endif
