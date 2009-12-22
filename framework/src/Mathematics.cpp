0a9b3234e2a47d0001c67594cf290c39a2922393
gcowan <gcowan@4525493e-7705-40b1-a816-d608a930855b>
2009-12-22 11:35:15 +0000

    Added Mathematics namespace. Empty for moment.

---
+++ framework/src/Mathematics.cpp
@@ -0,0 +1,16 @@
+/**
+	@namespace Mathematics
+
+	Namespace holding some common mathematical functions that are used
+	by many PDFs. For example, gaussian's convolved with trigonometric
+	functions.
+
+        @author Greig A Cowan greig.cowan@cern.ch
+        @date 2009-12-22
+*/
+
+namespace Mathematics 
+{
+
+}
+
