3bc3bd6ef15386672b1d84a15e5598c85c65e417
mneedham <mneedham@4525493e-7705-40b1-a816-d608a930855b>
2010-11-05 12:27:34 +0000

    Add AddRapidFitInfo.C

---
+++ scripts/AddRapidFitInfo.C
@@ -0,0 +1,72 @@
+#include <string>
+#include <iostream>
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TChain.h"
+#include "TMath.h"
+
+/* 
+*  Add the rapid fit variables to the standard ntuple. To use, type then root and at the prompt
+*   .L AddRapidFitInfo.C++
+*   AddRapidFitInfo("TheFile.root", "DecayTree")
+*   It will write out TheFile_RapidFit.root
+*/
+
+
+void AddRapidFitInfo(std::string fileName = "theFile.root", std::string treeName = "DecayTree"){
+
+  // get the input
+  TChain* decaytree = new TChain(treeName.c_str());
+  decaytree->Add(fileName.c_str());
+
+	
+  // make the output
+  std::string outputName = fileName.substr(0,fileName.size() - 5);
+  outputName += "_RapidFit.root";
+  TFile* outFile  =new TFile(outputName.c_str(),"RECREATE");
+   
+  std::cout << "Reading: " << treeName << " from " << fileName  << " to " << outputName << std::endl;
+
+  // clone the tree..
+  TTree*  newtree = decaytree->CloneTree(-1);
+ 
+  //add the new branches 
+  float time; TBranch* branch_time =  newtree->Branch("time",&time, "time/F");
+  float cosTheta; TBranch* branch_cosTheta =  newtree->Branch("cosTheta",&cosTheta, "cosTheta/F");
+  float cosPsi; TBranch* branch_cosPsi =  newtree->Branch("cosPsi",&cosPsi, "cosPsi/F");
+  float phi; TBranch* branch_phi =  newtree->Branch("phi",&phi, "phi/F");
+  int tag; TBranch* branch_tag =  newtree->Branch("tag",&tag, "tag/I");
+  float mistag; TBranch* branch_mistag =  newtree->Branch("mistag",&mistag, "mistag/F");
+  float mass; TBranch* branch_mass =  newtree->Branch("mass",&mass, "mass/F");
+
+  // address of the old variables
+  newtree->SetBranchAddress("B_s0_LOKI_DTF_CTAUS",&time); 
+  newtree->SetBranchAddress("B_s0_ThetaTr",&cosTheta); 
+  newtree->SetBranchAddress("B_s0_ThetaK",&cosPsi);
+  newtree->SetBranchAddress("B_s0_ThetaVtr",&phi);
+  newtree->SetBranchAddress("B_s0_TAGDECISION",&tag);
+  newtree->SetBranchAddress("B_s0_TAGOMEGA",&mistag);
+  newtree->SetBranchAddress("B_s0_LOKI_MASS_JpsiConstr",&mass);
+
+  // loop and make the conversion
+  int num_entries  = newtree->GetEntries();
+  for (int i = 0; i <num_entries; ++i) {
+    newtree->GetEntry(i);
+    time /= 0.299792458; 
+    cosTheta = TMath::Cos(cosTheta);
+    cosPsi = TMath::Cos(cosPsi);
+    branch_time->Fill();
+    branch_cosTheta->Fill(); 
+    branch_cosPsi->Fill();
+    branch_phi->Fill(); 
+    branch_tag->Fill(); 
+    branch_mistag->Fill(); 
+    branch_mass->Fill(); 
+  } // loop i
+
+  newtree->Write();
+  outFile->Close();
+
+}
