27a633f5d9a682f836f53b377f010e949c01e9ed
cofitzpa <cofitzpa@4525493e-7705-40b1-a816-d608a930855b>
2011-03-08 16:34:11 +0000

    added RNG seed and better ordering to feldman-cousins ganga script

--- scripts/Ganga_FC.py
+++ scripts/Ganga_FC.py
@@ -1,21 +1,22 @@
 from math import *
 
-
 #def fcsplitter(commonargs=['-f','XML_File.xml','--doFCscan'],toys=10,toyspersj=5,par1='x',par1min=0.,par1max=0.,par1res=1,par2='y',par2min=0.,par2max=0.,par2res=1):
-def fcsplitter(commonargs='-f XML_File.xml --doFCscan',toys=10,toyspersj=5,par1='x',par1min=0.,par1max=0.,par1res=1,par2='y',par2min=0.,par2max=0.,par2res=1):
+def fcsplitter(commonargs='-f XML_File.xml --doFCscan',toys=10,toyspersj=5,par1='x',par1min=0.,par1max=0.,par1res=1,par2='y',par2min=0.,par2max=0.,par2res=1,seed=0):
+	nseed = seed
         args = []
         step1 = abs(par1min-par1max)/float(par1res)
         step2 = abs(par2min-par2max)/float(par2res)
-        for i in range(0,par1res+1,1):
-                par1val = par1min + float(i)*step1
-                for j in range(0,par2res+1,1):
-                        par2val = par2min + float(j)*step2
-			for k in range(0,toys,toyspersj):
+	for k in range(0,toys,toyspersj):
+	        for i in range(0,par1res+1,1):
+                	par1val = par1min + float(i)*step1
+	                for j in range(0,par2res+1,1):
+        	                par2val = par2min + float(j)*step2
+				nseed = nseed + 1
                         	sjarg = []
-				sjarg.append(commonargs+' -repeats '+str(toyspersj)+' --defineContour '+ par1+','+str(par1val)+','+str(par1val)+',1 '+par2+','+str(par2val)+','+str(par2val)+',1')
+				sjarg.append(commonargs+' -repeats '+str(toyspersj)+' --defineContour '+ par1+','+str(par1val)+','+str(par1val)+',1 '+par2+','+str(par2val)+','+str(par2val)+',1'+' --useUUID --SetSeed '+str(nseed))
 				#sjarg.extend(commonargs)
 				#sjarg.extend(['-repeats',str(toyspersj),'--defineContour',par1+','+str(par1val)+','+str(par1val)+',1',par2+','+str(par2val)+','+str(par2val)+',1'])
-				print sjarg
+				#print sjarg
                         	args.append(sjarg)
         return args
 
@@ -39,9 +40,7 @@ name = 'Feldman_Cousins_test' ,
     ) ,
  outputdata = None ,
  splitter = ArgSplitter (
-
-#args = fcsplitter(['-f','/phys/linux/s0127440/sWave_scripts/FCXML.xml','--doFCscan'],10,10,'Phi_s',-pi,pi,1,'deltaGamma',-0.9,0.9,1)
- args = fcsplitter('-f /phys/linux/s0127440/sWave_scripts/FCXML.xml --doFCscan',200,20,'Phi_s',-pi,pi,20,'deltaGamma',-0.9,0.9,20)
+ args = fcsplitter('-f /phys/linux/s0127440/sWave_scripts/pete-unbiased-upper.xml --doFCscan',20,100,'Phi_s',0.0,2.0*pi,24,'deltaGamma',-0.9,0.9,24,2)
     ) ,
  backend = Condor ( ) 
     ) 
