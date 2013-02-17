89a1c10741bc9cc9948dd8d6a63012492347b7b4
rcurrie <rcurrie@4525493e-7705-40b1-a816-d608a930855b>
2013-02-17 18:16:57 +0000

    Actually adding RapidFitIntegratorConfig.h

---
+++ framework/include/RapidFitIntegratorConfig.h
@@ -0,0 +1,35 @@
+
+#pragma once
+#ifndef RAPIDFITINTEGRATOR_CONFIG_H
+#define RAPIDFITINTEGRATOR_CONFIG_H
+
+#define __DEFAULT_RAPIDFIT_FIXEDINTEGRATIONPOINTS 10000
+#define __DEFAULT_RAPIDFIT_USEGSL false
+#define __DEFAULT_RAPIDFIT_MAXINTEGRALSTEPS 1000000
+#define __DEFAULT_RAPIDFIT_INTABSTOL 1E-9
+#define __DEFAULT_RAPIDFIT_INTRELTOL 1E-9
+
+#include "Threading.h"
+
+using namespace::std;
+
+class RapidFitIntegratorConfig
+{
+	public:
+		RapidFitIntegratorConfig() : 
+			FixedIntegrationPoints( __DEFAULT_RAPIDFIT_FIXEDINTEGRATIONPOINTS ), useGSLIntegrator( __DEFAULT_RAPIDFIT_USEGSL ),
+			MaxIntegrationSteps( __DEFAULT_RAPIDFIT_MAXINTEGRALSTEPS ), IntegrationAbsTolerance( __DEFAULT_RAPIDFIT_INTABSTOL ),
+			IntegrationRelTolerance( __DEFAULT_RAPIDFIT_INTRELTOL ), numThreads( Threading::numCores() )
+		{
+		}
+
+		unsigned int FixedIntegrationPoints;
+		bool useGSLIntegrator;
+		unsigned int MaxIntegrationSteps;
+		double IntegrationAbsTolerance;
+		double IntegrationRelTolerance;
+		unsigned int numThreads;
+};
+
+#endif
+
