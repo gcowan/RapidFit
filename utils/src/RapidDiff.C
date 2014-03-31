7ec8efd398c43f1a90c6e650d4cc7617c3f7b15f
rcurrie <rcurrie@4525493e-7705-40b1-a816-d608a930855b>
2014-03-31 13:22:25 +0000

    Adding more columns to RapidFit

--- utils/src/RapidDiff.C
+++ utils/src/RapidDiff.C
@@ -95,15 +95,18 @@ int main( int argc, char* argv[] )
 
 	cout << setprecision( 8 ) << fixed << endl;
 	cout << setw( 20 ) << "Parameter     " << "   " << setw( 29 ) << " Result1      " << setw( 29 ) << " Result2      ";
-	cout << setw( 21 ) << " Diff in CV " << setw( 21 ) << " Error Ratio " << setw( 23 ) << " sqrt( |Err1^2-Err2^2| ) ";
-	cout << setw( 23 ) << "sqrt( Diff^2+DiffErr^2 )" << endl;
+	cout << setw( 21 ) << " Diff in CV " << setw( 21 ) << " Error Ratio " << "  " << setw( 23 ) << " sqrt(|Err1^2-Err2^2|) ";
+	cout << setw( 25 ) << " sqrt(|Err1^2-Err2^2|)/Err1 " << "  " << setw( 25 ) << " sqrt(|Err1^2-Err2^2|)/Err2 " << "  ";
+	cout << setw( 25 ) << "sqrt( Diff^2+DiffErr^2 )" << endl;
 
 	cout << endl;
 	for( unsigned int i=0; i< common.size(); ++i )
 	{
+		double ErrDiff = sqrt( fabs(Err_1[i]*Err_1[i] - Err_2[i]*Err_2[i]) );
 		cout << setw( 20 ) << common[i] << " : " << setw( 13 ) << CV_1[i] << " ± " << setw( 13 ) << Err_1[i] << "\t" << setw( 13 ) << CV_2[i] << " ± " << setw( 13 ) << Err_2[i] << "   ";
-		cout << setw( 15 ) << CV_diffs[i] << "   " <<  setw( 15 ) << Err_1[i] / Err_2[i] << "   " << setw( 23 ) << sqrt( fabs(Err_1[i]*Err_1[i] - Err_2[i]*Err_2[i]) );
-		cout << "   " << setw( 23 ) << sqrt( CV_diffs[i]*CV_diffs[i] + fabs(Err_1[i]*Err_1[i] - Err_2[i]*Err_2[i]) );
+		cout << setw( 15 ) << CV_diffs[i] << "   " <<  setw( 15 ) << Err_1[i] / Err_2[i] << "  " << setw( 23 ) << ErrDiff;
+		cout << setw( 25 ) << ErrDiff/Err_1[i] << "  " << setw( 25 ) << ErrDiff/Err_2[i];
+		cout << "  " << setw( 23 ) << sqrt( CV_diffs[i]*CV_diffs[i] + ErrDiff*ErrDiff );
 		cout << endl;
 	}
 	cout << endl;
@@ -128,24 +131,31 @@ string buildLatexTable( vector<TString> params, vector<double> CV1, vector<doubl
 	thisTable << "\\documentclass[a4paper,10pt]{article}" << endl;
 	thisTable << "\\usepackage[utf8]{inputenc}" << endl;
 	thisTable << "\\usepackage[landscape]{geometry}" << endl;
+	thisTable << "\\usepackage{amsmath}" << endl;
 	thisTable << endl;
 	thisTable << "\\begin{document}" << endl;
 	thisTable << endl;
 	thisTable << "\\renewcommand{\\arraystretch}{1.25}" << endl;
 	thisTable << "\\begin{table}" << endl;
-	thisTable << "\\begin{tabular}{@{}l|c|c|c|c|c|c@{}}" << endl;
+	thisTable << "\\hspace*{-3.cm}\\begin{tabular}{@{}l|c|c|c|c|c|c|c|c@{}}" << endl;
 	thisTable << setw( 30 ) << "Parameter   & " << "   " << setw( 29 ) << " Result1    & " << setw( 29 ) << " Result2    & ";
-	thisTable << setw( 18 ) << " Diff in CV & " << setw( 18 ) << " Error Ratio & " << setw( 20 ) << " $\\sqrt{ \\left|Err1^2-Err2^2\\right| }$";
-	thisTable << setw( 23 ) << " & $\\sqrt{ CVDiff^2+ErrDiffSq }$ " << "   \\\\ \\hline" << endl;
+	thisTable << setw( 15 ) << " Diff in CV & " << setw( 10 ) << " Error Ratio & " << setw( 20 ) << " $\\sqrt{ \\left|E1^2-E2^2\\right| }$";
+	thisTable << setw( 25 ) << " & $\\dfrac{\\sqrt{ \\left|E1^2-E2^2\\right| } }{ E1 }$ " << " & " << setw( 25 ) << " $\\dfrac{\\sqrt{ \\left|E1^2-E2^2\\right| } }{ E2 }$ ";
+	thisTable << setw( 23 ) << " & $\\sqrt{ \\left(\\Delta{CV}\\right)^2+\\Delta{E^2} }$ " << "   \\\\ \\hline" << endl;
 
 	for( unsigned int i=0; i< params.size(); ++i )
 	{
 
+		double ErrDiff = sqrt( fabs(Err1[i]*Err1[i] - Err2[i]*Err2[i]) );
+		double CVDiff = CV1[i]-CV2[i];
 		thisTable << setw( 30 ) << EdStyle::GetParamLatexName( params[i] ) << " & " << setw( 13 ) << CV1[i] << " $\\pm$ " << setw( 13 ) << Err1[i] << " & ";
 		thisTable << setw( 13 ) << CV2[i] << " $\\pm$ " << setw( 13 ) << Err2[i] << " & ";
-		thisTable << setw( 18 ) << CV1[i]-CV2[i] << " & " << setw( 18 ) << Err1[i] / Err2[i] << " & " << setw( 20 ) << sqrt( fabs(Err1[i]*Err1[i] - Err2[i]*Err2[i]) );
-		thisTable << " & " << setw( 23 ) << sqrt( (CV1[i]-CV2[i])*(CV1[i]-CV2[i]) + fabs(Err1[i]*Err1[i] - Err2[i]*Err2[i]) );
-		thisTable << "   \\\\" << endl;
+		thisTable << setw( 13 ) << CVDiff << " & " << setw( 13 ) << Err1[i] / Err2[i] << " & " << setw( 13 ) << ErrDiff;
+		if( fabs( ErrDiff / Err1[i] ) > 0.1 || fabs( ErrDiff / Err2[i] ) > 0.1 ) thisTable << " & \\textbf{" << setw( 13 ) << ErrDiff / Err1[i] << "} & \\textbf{" << setw( 13 ) << ErrDiff / Err2[i] << "}";
+		else thisTable << " & " << setw( 13 ) << ErrDiff / Err1[i] << " & " << setw( 13 ) << ErrDiff / Err2[i];
+		thisTable << " & " << setw( 13 ) << sqrt( CVDiff*CVDiff + ErrDiff*ErrDiff );
+		thisTable << "   \\\\";
+		thisTable << endl;
 
 	}
 
