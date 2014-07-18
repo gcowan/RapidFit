4fbb8b2933a54a00fd4a00bed439bd4cdeebf7a1
cofitzpa <cofitzpa@4525493e-7705-40b1-a816-d608a930855b>
2014-07-18 15:52:08 +0000

    made RapidToyDiff more better

--- utils/src/RapidToyDiff.C
+++ utils/src/RapidToyDiff.C
@@ -28,7 +28,46 @@
 
 using namespace::std;
 
-string buildLatexTable( vector<TString> params, vector<double> CV1, vector<double> Err1, vector<double> CV2, vector<double> Err2 );
+void MakePlot(TH1* hist, TString Name, TString dir){
+
+	Histogram_Processing::OptimallyRebin( hist );
+
+	TString FitType = Histogram_Processing::Best_Fit_Function( hist );
+	Histogram_Processing::Silent_Fit( hist, FitType );
+	TString Canvas_Name(Name+"_Canvas");  
+	TCanvas* c1 = EdStyle::RapidFitCanvas( Canvas_Name, Canvas_Name );
+	Histogram_Processing::Silent_Draw( c1, hist );
+	TAxis* x_axis = hist->GetXaxis();
+	string units;
+	units = " " + EdStyle::GetParamRootUnit( EdStyle::Remove_Suffix(Name) );
+	x_axis->SetTitle( EdStyle::GetParamRootName( EdStyle::Remove_Suffix(Name )) + " " + EdStyle::Get_Suffix(Name) + units.c_str() );
+	hist->SetTitle( "Toy Study " + EdStyle::GetParamRootName( Name ) + " difference" );
+
+	c1->Update();
+
+	Histogram_Processing::Silent_Print( c1, dir+"/"+Name+".png" );
+	Histogram_Processing::Silent_Print( c1, dir+"/"+Name+".pdf" );
+	Histogram_Processing::Silent_Print( c1, dir+"/"+Name+".C" );
+
+	TPaveStats* thisStats = (TPaveStats*)hist->GetListOfFunctions()->FindObject("stats");
+	thisStats->SetFillStyle(3023);
+	thisStats->SetTextColor(1);
+
+	thisStats->Draw();
+
+	c1->Update();
+
+	TF1* fitted = hist->GetFunction( FitType );
+
+	Histogram_Processing::Silent_Print( c1, dir+"/"+Name+"_c_thru.png" );
+	Histogram_Processing::Silent_Print( c1, dir+"/"+Name+"_c_thru.pdf" );
+	Histogram_Processing::Silent_Print( c1, dir+"/"+Name+"_c_thru.C" );
+
+
+
+
+
+}
 
 int main( int argc, char* argv[] )
 {
@@ -40,6 +79,8 @@ int main( int argc, char* argv[] )
 		cout << endl;
 		exit(-1);
 	}
+	     gStyle->SetOptStat(0);
+	             gStyle->SetOptFit(111);
 
 	TString Output_Dir;
 	Output_Dir = "ToyStudy_Output_"; Output_Dir.Append( StringOperations::TimeString() );
@@ -79,6 +120,9 @@ int main( int argc, char* argv[] )
 		TString pullStr( common[i] );
 		pullStr.Append( "_pull" );
 
+		TString ptpullStr(common[i]);
+		ptpullStr.Append("_perToyPull");
+
 		vector<Double_t>* thisValue1 = TTree_Processing::Buffer_Branch( RapidFitResult_1, valueStr );
 		vector<Double_t>* thisError1 = TTree_Processing::Buffer_Branch( RapidFitResult_1, errorStr );
 		vector<Double_t>* thisPull1 = TTree_Processing::Buffer_Branch( RapidFitResult_1, pullStr );
@@ -87,22 +131,39 @@ int main( int argc, char* argv[] )
 		vector<Double_t>* thisError2 = TTree_Processing::Buffer_Branch( RapidFitResult_2, errorStr );
 		vector<Double_t>* thisPull2 = TTree_Processing::Buffer_Branch( RapidFitResult_2, pullStr );
 
-		TString ValueName(valueStr);
 
-		TH1D* deltaValue = new TH1D(valueStr,valueStr,1000,-1.0,1.0);
-		deltaValue->SetName( ValueName );
 
+		TH1D* deltaValue = new TH1D(valueStr,valueStr,1000,1.0,-1.0);
+		deltaValue->SetName( valueStr );
+		
+		TH1D* deltaError = new TH1D(errorStr,errorStr,1000,1.0,-1.0);
+		deltaError->SetName( errorStr );
+
+		TH1D* deltaPull = new TH1D(pullStr,pullStr,1000,1.0,-1.0);
+		deltaPull->SetName( pullStr );
+
+		TH1D* perToyPull = new TH1D(ptpullStr,ptpullStr,1000,1.0,-1.0);
+		perToyPull->SetName( ptpullStr );
+	
 
 		cout << "Number of entries in vectors: " << thisValue1->size() << endl;
 		for(unsigned int j=0; j< thisValue1->size(); j++){
 
 			deltaValue->Fill(thisValue1->at( j ) - thisValue2->at( j ));
-			//deltaError->Fill(thisError1->at( j ) - thisError2->at( j ));
-			//deltaPull->Fill(thisPull1->at( j ) - thisPull2->at( j ));
+			deltaError->Fill(thisError1->at( j ) - thisError2->at( j ));
+
+			deltaPull->Fill(thisPull1->at( j ) - thisPull2->at( j ));
+			perToyPull->Fill((thisValue1->at( j ) - thisValue2->at( j ))/(thisError1->at( j )));
 
 
 
 		}
+
+
+		MakePlot(deltaValue, valueStr, Output_Dir);
+		MakePlot(deltaError, errorStr, Output_Dir);
+		MakePlot(deltaPull, pullStr, Output_Dir);
+		MakePlot(perToyPull, ptpullStr, Output_Dir);
 
 		delete thisValue1;
 		delete thisError1;
@@ -112,30 +173,11 @@ int main( int argc, char* argv[] )
 		delete thisPull2;
 
 
-		Histogram_Processing::OptimallyRebin( deltaValue );
-
-		TString ValueFitType = Histogram_Processing::Best_Fit_Function( deltaValue );
-		Histogram_Processing::Silent_Fit( deltaValue, ValueFitType );
-		TString Canvas_Name(valueStr+"_Canvas");  
-		TCanvas* c1 = EdStyle::RapidFitCanvas( Canvas_Name, Canvas_Name );
-		Histogram_Processing::Silent_Draw( c1, deltaValue );
-		TAxis* x_axis = deltaValue->GetXaxis();
-		string units;
-		units = " " + EdStyle::GetParamRootUnit( EdStyle::Remove_Suffix(valueStr) );
-		x_axis->SetTitle( EdStyle::GetParamRootName( EdStyle::Remove_Suffix(valueStr )) + " " + EdStyle::Get_Suffix(valueStr) + units.c_str() );
-		deltaValue->SetTitle( "Toy Study " + EdStyle::GetParamRootName( valueStr ) + " difference" );
-
-				c1->Update();
-
-				Histogram_Processing::Silent_Print( c1, Output_Dir+"/"+valueStr+".png" );
-				Histogram_Processing::Silent_Print( c1, Output_Dir+"/"+valueStr+".pdf" );
-				Histogram_Processing::Silent_Print( c1, Output_Dir+"/"+valueStr+".C" );
 
 
+	}
 
+}
 
-				}
-
-				}
 
 
